793ffd70b2979773c2c8b1e39c50eef440d5288e
phx1314 <1163096519@qq.com>
2019-06-16 21:14:15 +0800

    78

--- app/src/main/java/com/ndtlg/dbbx/bean/BeanAddYj.java
+++ app/src/main/java/com/ndtlg/dbbx/bean/BeanAddYj.java
@@ -1,5 +1,7 @@
 package com.ndtlg.dbbx.bean;
 
+import android.text.TextUtils;
+
 import java.io.Serializable;
 
 /**
@@ -9,6 +11,7 @@ import java.io.Serializable;
 public class BeanAddYj implements Serializable {
     public int action = 60004;
     public String content;
+    public int uid = TextUtils.isEmpty(com.ndtlg.dbbx.F.uid) ? 0 : Integer.valueOf(com.ndtlg.dbbx.F.uid);
 
     public BeanAddYj(String content) {
         this.content = content;
